c2665423770b7a7bc48dc716fe1b4e3b6e0aa389
Akdiel Callejas <Akdiel.callejas21@gmail.com>
2020-11-14 15:30:13 -0600

    Se agregó trasparencia al navigation y se cambió el color de la flecha

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { Form } from "native-base"
+import { Form, Image } from "native-base"
+import {StyleSheet, Dimensions, Vi} from "react-native"
 import React from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import homeScreen from "./src/screens/homeScreen";
 import recipeInfoScreen from "./src/screens/recipeInfoScreen";
 import recipesSearchScreen from "./src/screens/recipesSearchScreen";
+
+const { width, height } = Dimensions.get("window");
 
 const Stack = createStackNavigator();
 
@@ -13,9 +16,28 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="homeScreen">
         <Stack.Screen name="homeScreen" component={homeScreen} options={{headerShown: false}}></Stack.Screen>
-        <Stack.Screen name="recipesSearchScreen" component={recipesSearchScreen} options={{title: ' '}}></Stack.Screen>
-        <Stack.Screen name="recipeInfoScreen" component={recipeInfoScreen} options={{title: ' '}}></Stack.Screen>
+        <Stack.Screen name="recipesSearchScreen" component={recipesSearchScreen}
+          options={{title: ' ',
+             headerTransparent: true,
+             headerTintColor: 'red'
+             }}>
+          </Stack.Screen>
+        <Stack.Screen name="recipeInfoScreen" component={recipeInfoScreen} 
+          options={{title: ' ',
+            headerTransparent: true,
+            headerTintColor: 'red'
+          }}>
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  logoApp: {
+    width: width,
+    height: height * 0.08,
+    resizeMode: "contain",
+    marginTop: 30
+  },
+});
