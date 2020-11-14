4f24f04f9066c529730c7a1b6ddd8f2c8601f8b1
Akdiel Callejas <Akdiel.callejas21@gmail.com>
2020-11-14 15:35:31 -0600

    eliminación de elementos no necesarios

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
-import { Form, Image } from "native-base"
-import {StyleSheet, Dimensions, Vi} from "react-native"
 import React from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import homeScreen from "./src/screens/homeScreen";
 import recipeInfoScreen from "./src/screens/recipeInfoScreen";
 import recipesSearchScreen from "./src/screens/recipesSearchScreen";
-
-const { width, height } = Dimensions.get("window");
 
 const Stack = createStackNavigator();
 
@@ -32,12 +28,3 @@ export default function App() {
     </NavigationContainer>
   )
 };
-
-const styles = StyleSheet.create({
-  logoApp: {
-    width: width,
-    height: height * 0.08,
-    resizeMode: "contain",
-    marginTop: 30
-  },
-});
